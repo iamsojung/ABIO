073ece381cd75e8723df7e3abc17fc206d42f04a
jhpark <parkjh1@mih.co.kr>
2022-02-21 19:33:32 +0900

    문자열 압축 preview

--- JH/1.문자열압축/PRM_60057.js
+++ JH/1.문자열압축/PRM_60057.js
@@ -1,4 +1,19 @@
+const s = "ababcdcdababcdcd";
+
 function solution(s) {
     var answer = 0;
+    for(var i=1; i<s.length+1; i++){
+        var result = "";
+        var str ="";
+        for(var j=0; j<Math.ceil(s.length/i); j++){
+            if(str === s.substr(j*i,i)){
+                
+            }
+            console.log(s.substr(j*i,i));
+            str = s.substr(j*i,i);
+        }
+    }
     return answer;
 }
+
+//문자열 자르기 > 문자열 압축 > 압축 사이즈 비교 
