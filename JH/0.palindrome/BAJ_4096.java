93325a1ff54788091ac772a8e4ef2d2a91a931bd
jhpark <parkjh1@mih.co.kr>
2022-02-21 18:25:56 +0900

    소스 정리

--- JH/0.palindrome/BAJ_4096.java
+++ JH/0.palindrome/BAJ_4096.java
@@ -22,14 +22,22 @@ public class BAJ_4096 {
         }
     }
 
+    // public static boolean isCheck(String value){
+    //     StringBuffer str = new StringBuffer(value);
+    //     if(value.equals(str.reverse().toString())){
+    //         return false;
+    //     }else{
+    //         return true;
+    //     }
+    // }
     public static boolean isCheck(String value){
-        StringBuffer str = new StringBuffer(value);
-        if(value.equals(str.reverse().toString())){
-            return false;
-        }else{
-            return true;
+        for (int i = 0; i < value.length()/2; i++) {
+            if(value.charAt(i) != value.charAt(value.length()-i-1)){
+                return false;
+            }
         }
-    }
+        return true;
+    }   
 
     public static String addZero(int intValue, int sizeGap){
         String addZeroValue = Integer.toString(intValue);
