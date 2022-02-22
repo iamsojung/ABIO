baa57836b4f60e2b05d7c94ba652843f8eaa5eac
jhpark <parkjh1@mih.co.kr>
2022-02-22 20:18:39 +0900

    java set

--- JH/1.문자열압축/PRM_60057.java
+++ JH/1.문자열압축/PRM_60057.java
@@ -1,3 +1,34 @@
-public class PRM_60057 {
-    
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        for(int i=1; i<s.length()+1; i++){
+            String str ="";
+            String result = "";
+            int cnt = 1;
+            for(int j=0; j<=Math.ceil(s.length()/i); j++){
+                int k = i*j+i;
+                if(k > s.length()){
+                    k = s.length();
+                }
+
+                if(j==0){
+                }else if(str.equals(s.substring(i*j,k))){
+                    cnt++;
+                }else{
+                    if(cnt==1){ 
+                        result += str;
+                    }else{
+                        result += String.valueOf(cnt) + str;
+                        
+                    }
+                    cnt = 1;
+                }
+                str = s.substring(i*j,k);
+            }
+            if(answer == 0 || answer > result.length()){
+                answer = result.length();
+            }
+        }
+        return answer;
+    }
 }
