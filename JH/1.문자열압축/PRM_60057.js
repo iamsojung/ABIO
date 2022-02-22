a84f56e874a6650dfd58c09c71d61edb4402a3a2
jhpark <parkjh1@mih.co.kr>
2022-02-22 19:29:19 +0900

    complete js

--- JH/1.문자열압축/PRM_60057.js
+++ JH/1.문자열압축/PRM_60057.js
@@ -29,3 +29,24 @@ function solution(s) {
     return answer;
 }
 console.log("@@: "+solution1(s));
+
+//다른사람 풀이보기 
+//https://programmers.co.kr/learn/courses/30/lessons/60057/solution_groups?language=javascript
+
+const solution = s => {
+  const range = [...Array(s.length)].map((_, i) => i + 1);
+  return Math.min(...range.map(i => compress(s, i).length));
+};
+
+const compress = (s, n) => {
+  const make = ([a, l, c]) => `${a}${c > 1 ? c : ''}${l}`;
+  return make(
+    chunk(s, n).reduce(
+      ([a, l, c], e) => e === l ? [a, l, c + 1] : [make([a, l, c]), e, 1],
+      ['', '', 0]
+    )
+  );
+};
+
+const chunk = (s, n) =>
+  s.length <= n ? [s] : [s.slice(0, n), ...chunk(s.slice(n), n)];
