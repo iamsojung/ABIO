f104d1602527267d5fa0ee65019d99fc65b9392a
jhpark <parkjh1@mih.co.kr>
2022-02-22 19:25:25 +0900

    complete js

--- JH/1.문자열압축/PRM_60057.js
+++ JH/1.문자열압축/PRM_60057.js
@@ -1,31 +1,31 @@
 // node ./JH/1.문자열압축/PRM_60057.js
 
-const s = "aabbaccc";
+const s = "ababcdcdababcdcd";
 
 function solution(s) {
     var answer = 0;
-    for(var i=1; i<s.length+1; i++){
-        var result = "";
-        var str ="";
-        var cntMap = new Map();
-        for(var j=0; j<=Math.ceil(s.length/i); j++){
-            if(str === s.substr(j*i,i)){
-                cntMap.set(str,cntMap.get(str)+1);
+    var len = s.length;
+    //문자열 가공
+    for(var i=1; i<len+1; i++){
+        var str="";
+        var cnt=1;
+        var result ="";
+        for(var j=0; j<=Math.ceil(len/i); j++){
+            if(j == 0){
+            }else if(str === s.substr(i*j,i)){
+                cnt++;
             }else{
-                cntMap.set(s.substr(j*i,i),1);
-                if(j>0){
-                    result += cntMap.get(str) + str;
-                }
+                if(cnt==1) cnt = "";
+                result += cnt + str;
+                cnt = 1;
             }
-            str = s.substr(j*i,i);
+            str = s.substr(i*j,i);
         }
-        result = result.replace(/1/gi, "");
+        console.log("##: "+result);
         if(answer == 0 || answer > result.length){
             answer = result.length;
         }
     }
     return answer;
 }
-
-console.log("@@: "+solution(s));
-//문자열 자르기 > 문자열 압축 > 압축 사이즈 비교 
+console.log("@@: "+solution1(s));
