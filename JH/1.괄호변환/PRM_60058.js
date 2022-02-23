6cba5a9c8185d1b7bac3a342b42a3a3318c135cc
jhpark <parkjh1@mih.co.kr>
2022-02-23 22:13:11 +0900

    js low accuration

--- JH/1.괄호변환/PRM_60058.js
+++ JH/1.괄호변환/PRM_60058.js
@@ -7,26 +7,59 @@
 
 
 function solution(p) {
-    
-    var answer = '';
-    var flag = false;
-    var trs = p.replace(/\(/gi,"0").replace(/\)/gi,"1");
-    isCheck(trs);
-    if(flag){
+    if(isCheck(p)){
         return p;
     }else{
-        trs = trs.split('').sort().join('');
-        isCheck(trs);
-    }
-    console.log(answer);
-    return answer;
-    
-    function isCheck(str){
-            for(var i=0; i<p.length/2; i++){
-        trs = trs.split("01").join('');
-        if(trs == ''){
-            flag = true;
+        const arridx = getIdx(p);
+        //console.log(arridx);
+        const arrp = p.split('');
+        const rep = getSort(p);
+        const answer="";
+        if(p.length > 2){
+        var cnt = 0;
+           for(var k=0; k<p.length; k++){
+            if(arridx.indexOf(k) === -1){
+                //console.log(k);
+                //console.log(rep[cnt]);
+                arrp[k] = rep[cnt];
+                cnt++;
+                }
+            } 
+        }else{
+            return rep; 
         }
+        return arrp.join('');
     }
+    
+    
+    //console.log(getIdx(p));
+    function getIdx(p){
+        const arr = p.split('');
+        const reArr = [];
+        for(var i=0; i<p.length; i++){
+            if(arr[i] === '(' && arr[i+1] === ')'){
+                reArr.push(i);
+                reArr.push(i+1);
+            }
+        }
+        return reArr;
+    }
+    function getSort(p){
+        //const trs = p.replace(/\(/gi,"0").replace(/\)/gi,"1");
+        const str = p.split("()").join('').split('').sort().join('');
+        return str;
+
+    }
+        function isCheck(str){
+            var trs = str;
+            for(var i=0; i<str.length/2; i++){
+                trs = trs.split("()").join('');
+            }
+            if(trs === ''){
+                return true;
+            }else{ 
+                return false;
+            }
+  
     }
 }
